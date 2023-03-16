5e6baf4a9692a58764d93ac1a185fb02d59117f8
seoyamin <seoyamin@ewhain.net>
2023-03-16 20:38:03 +0900

    [과제 1] 3-5 작성

---
+++ Assignment01/Assignment01.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILE_NAME "testdata.txt"
+
+#define STsize 1000 // size of string table
+#define HTsize 100  // size of hash table
+
+#define FALSE 0
+#define TRUE 1
+
+#define isLetter(x) ( ((x) >= 'a' && (x) <= 'z') || ((x) >= 'A' && (x) <= 'Z') )]
+#define isDigit(x) ( (x) >= '0' && (x) <= '9' )
+
+typedef struct HTentry *HTpointer,
+typedef struct HTentry {
+	int index;       // index of identifier in ST
+	HTpointer next;  // pointer to next identifier
+} HTentry;
+
+
+enum errorTypes { noerror, illsp, illid, overst };
+typedef enum errorTypes ERRORtypes;
+
+char seperators[] = ".,;:?!\t\n";
+
+HTpointer HT[HTsize];
+char ST[STsize];
+
+int nextid = 0;    // the current identifier 
+int nextfree = 0;  // the next available index of ST
+
+int hashcode;      // hash code of identifier
+int sameid;        // first index of identifier
+
+int found;         // for the previous occurrence of an identifie
+
+ERRORtypes err;
+
+FILE* fp;          // to be a pointer to FILE
+char input;
+
+
+// Initialize - open input file
+
+void initialize() {
+	fp = fopen(FILE_NAME, "r");
+	input = fget(fp);
+}
+
+
+// is Seperator - distinguish the seperator
+
+int isSeperator(char c) {
+	int i;
+	int sep_len;
+
+	sep_len = strlen(seperators);
+	
+	for (i = 0; i < sep_len; i++) {
+		if (c == seperators[i])
+			return 1;
+	}
+
+	return 0;
+}
+
+
+// print Heading
+
+void PrintHeading() {
+	printf("\n\n");
+	printf("  -----------     ----------- \n");
+	printf("  Index in ST      identifier  \n");
+	printf("  -----------     ----------- \n");
+	printf("\n");
+}
+
+
+// Print HS table
+
+void PrintHStable() {
+	int i, j;
+	HTpointer here;
+
+	printf("\n\n\n\n\n [[ HASH TABLE ]] \n\n");
+
+	for (i = 0; i < HTsize; i++) {
+		if (HT[i] != NULL) {
+			printf(" Hash Code %3d : ", i);
+			for (here = HT[i]; here != NULL; here = here->next) {
+				j = here->index;
+				while (ST[j] != '\0' && j < STsize)
+					printf("%c", ST[j++]);
+				printf("    ");
+			}
+			printf("\n");
+		}
+		printf("\n\n\n < %5d characters are used in the string table > \n", nextfree);
+	}
+}
+

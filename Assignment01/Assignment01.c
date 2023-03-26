b70b99da2ee018cc79b56ffc2453bc5786424bac
Eunbi <108458468+eunbi-jeong@users.noreply.github.com>
2023-03-26 11:46:59 +0900

    EB: Add comments to Assignment01.c

--- Assignment01/Assignment01.c
+++ Assignment01/Assignment01.c
@@ -203,23 +203,23 @@ void ComputeHS(int nid, int nfree) {
 	hashcode = code % HTsize;
 }
 
-// LookupHs 
+// LookupHs - Hash Table을 보고 현재 읽어들인 identifier의 존재 여부를 판단하는 함수
 
 void LookupHS(int nid, int hscode)
 {
 	HTpointer here;
 	int i, j;
-
-	found = FALSE;
-	if (HT[hscode] != NULL) {
+	found = FALSE; 	// Hash Table을 읽기 전, FALSE로 초기화
+	
+	if (HT[hscode] != NULL) {  // hash code 위치에 어떠한 문자라도 존재하는 경우
 		here = HT[hscode];
-		while (here != NULL && found == FALSE) {
+		while (here != NULL && found == FALSE) {  // 현재 가리키는 위치에 문자가 있고 아직 identifier가 발견되지 않은 경우
 			found = TRUE;
 			i = here->index;
 			j = nid;
 			sameid = i;
 
-			while (ST[i] != '\0' && ST[j] != '\0' && found == TRUE) {
+			while (ST[i] != '\0' && ST[j] != '\0' && found == TRUE) { // 문자를 하나씩 비교하면서 identifier의 일치 여부 판단
 				if (ST[i] != ST[j])
 					found = FALSE;
 				else {
@@ -227,25 +227,26 @@ void LookupHS(int nid, int hscode)
 					j++;
 				}
 			}
-			here = here->next;
+			here = here->next;  // linked list의 다음 identifier로 이동
 		}
 	}
 }
 
-// ADDHT
+// ADDHT - Hash Table에 identifier를 추가하는 함수
 
 void ADDHT(int hscode)
 {
 	HTpointer ptr;
 
 	ptr = (HTpointer)malloc(sizeof(ptr));
-	ptr->index = nextid;
+	ptr->index = nextid;	
 	ptr->next = HT[hscode];
-	HT[hscode] = ptr;
+	HT[hscode] = ptr; // linked list로써 identifier 삽입
 }
 
 
-// CHECK
+// CHECK - 케이스를 확인하고 적절한 함수를 호출하는 함수 
+
 void check() {
 
 	// [case 1] 더이상 읽을 문자가 없는 경우
@@ -260,7 +261,7 @@ void check() {
 		ST[nextfree++] = '\0';
 		err = noerror;
 
-		int len = nextfree - nextid - 1;
+		int len = nextfree - nextid - 1;  // 현재 읽어들인 identifier의 길이
 
 		// [case 2-1] OVERLEN
 		if (len > 10) {
@@ -274,14 +275,14 @@ void check() {
 			ComputeHS(nextid, nextfree);
 			LookupHS(nextid, hashcode);
 
-			if (!found) {
+			if (!found) { // Hash Table에 동일한 identifier가 존재하지 않는 경우
 				printf("%6d			", nextid);
 				for (i = nextid; i < nextfree - 1; i++)
 					printf("%c", ST[i]);
 				printf("		(entered)\n");
 				ADDHT(hashcode);
 			}
-			else {
+			else { // Hash Table에 동일한 identifier가 존재하는 경우
 				printf("%6d			", sameid);
 				for (i = nextid; i < nextfree - 1; i++)
 					printf("%c", ST[i]);
@@ -300,7 +301,7 @@ void check() {
 
 	}
 
-	// [case 4] ILLID2 : NOT ALLOWED IDENTIFIER
+	// [case 4] ILLID2 : not allowed identifier
 	else {
 		if (nextfree == STsize) {
 			err = overst;
@@ -314,19 +315,19 @@ void check() {
 }
 
 
-// MAIN
+// MAIN - main 
 
 int main()
 {
 	PrintHeading();
 	initialize();
 
-	while (input != EOF) {
-		err = noerror;
+	while (input != EOF) { // input이 존재하는 동안 문자를 읽으며 반복
+		err = noerror;  // 에러 초기화
 		SkipSeperators();
 		ReadIO();
 
-		if (input == EOF) {
+		if (input == EOF) { // EOF이면 check() 함수를 호출하고 while문 종료
 			check();
 			break;
 		}

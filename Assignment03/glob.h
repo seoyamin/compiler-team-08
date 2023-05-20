9d924fa0126e0921702bf9d258819a92ae299609
mingyo choi <128064550+mingyo-choi@users.noreply.github.com>
2023-05-21 00:30:38 +0900

    Update glob.h
    0520 : update

--- Assignment03/glob.h
+++ Assignment03/glob.h
@@ -1,7 +1,7 @@
 /*
-* glob.h –  프로젝트의 전역 변수들
-* progrmmer – 김민서, 정은비, 최민교
-* date - 26/04/2023
+* glob.h - 프로젝트의 전역 변수들
+* progrmmer - 김민서, 정은비, 최민교
+* date - 30/05/2023
 */
 extern int currid;				// the current identifier 
 extern unsigned int currlinenum;	// current line number
@@ -9,3 +9,23 @@ extern unsigned int cErrors;	// 등장한 에러 횟수
 
 enum errortypes;				// 에러 타입 enum 
 enum errortypes et;
+
+#define STsize 1000		// String Table(ST)의 size
+#define HTsize 100		// Hash Table(HT)의 size
+
+#define FALSE 0
+#define TRUE 1
+
+/* global variables from symtable.c */
+typedef struct HTentry* HTpointer;
+typedef struct HTentry {
+	int index;             // ST상에서 identifier의 인덱스
+	HTpointer next;        // 다음 identifier를 위한 포인터
+	char *type;	       // identifier의 속성
+	int linenum;		// identifier의 linenum (We should add the initialization code for this variable.)
+} HTentry;
+
+HTpointer HT[HTsize];
+char ST[STsize];
+
+char* identStr;

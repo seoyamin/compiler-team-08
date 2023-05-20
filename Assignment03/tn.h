b68bf0ecfb49a4429bb88675141d49d608cb0155
mingyo choi <128064550+mingyo-choi@users.noreply.github.com>
2023-05-21 00:32:42 +0900

    0520:Update tn.h

--- Assignment03/tn.h
+++ Assignment03/tn.h
@@ -1,15 +1,48 @@
-/*
-* tn.h – token type 
-* programmer – 김민서, 정은비, 최민교
-* date - 05/30/2023
-*/
-enum tokentypes {
-	TEOF, TCONST, TELSE, TIF, TINT, TRETURN,
-	TVOID, TWHILE, TADD, TSUB, TMUL,
-	TDIV, TMOD, TASSIGN, TADDASSIGN, TSUBASSIGN,
-	TMULASSIGN, TDIVASSIGN, TMODASSIGN, TNOT, TAND,
-	TOR, TEQUAL, TNOTEQU, TLESS, TGREAT,
-	TLESSE, TGREATE, TINC, TDEC, TLEFTPAR,
-	TRIGHTPAR, TCOLON, TLEFTBRACE, TRIGHTBRACE, TLEFTBRACKET,
-	TRIGHTBRACKET, TSEMI, TNUMBER, TIDENT, TERROR
-};
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	TLOWERTHANELSE	258
+#define	TELSE	259
+#define	TEOF	260
+#define	TCONST	261
+#define	TIF	262
+#define	TINT	263
+#define	TRETURN	264
+#define	TVOID	265
+#define	TWHILE	266
+#define	TADD	267
+#define	TSUB	268
+#define	TMUL	269
+#define	TDIV	270
+#define	TMOD	271
+#define	TASSIGN	272
+#define	TADDASSIGN	273
+#define	TSUBASSIGN	274
+#define	TMULASSIGN	275
+#define	TDIVASSIGN	276
+#define	TMODASSIGN	277
+#define	TNOT	278
+#define	TAND	279
+#define	TOR	280
+#define	TEQUAL	281
+#define	TNOTEQU	282
+#define	TLESS	283
+#define	TGREAT	284
+#define	TLESSE	285
+#define	TGREATE	286
+#define	TINC	287
+#define	TDEC	288
+#define	TLEFTPAR	289
+#define	TRIGHTPAR	290
+#define	TCOLON	291
+#define	TLEFTBRACE	292
+#define	TRIGHTBRACE	293
+#define	TLEFTBRACKET	294
+#define	TRIGHTBRACKET	295
+#define	TSEMI	296
+#define	TNUMBER	297
+#define	TIDENT	298
+#define	TERROR	299
+
+
+extern YYSTYPE yylval;

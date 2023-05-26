2ef616b335999bf3b3d0330caae1933919dfa8ac
Eunbi <108458468+eunbi-jeong@users.noreply.github.com>
2023-05-26 16:56:22 +0900

    EB: Update reporterror.c
    I added the 'printerror()' function and modified the 'reporterror()' function.

--- Assignment03/reporterror.c
+++ Assignment03/reporterror.c
@@ -14,48 +14,78 @@
 #define isLetter(x) (((x) >= 'a' && (x) <='z') ||((x) >= 'A' && (x) <='Z') || ((x)=='_'))
 #define isDigit(x) (((x) >= '0' && (x) <='9'))
 
-// 에러 타입 enum
-enum errortypes {
-	OVERLEN, ILLCHAR, ILLID
-};
-
 // reporterror - 에러케이스 별로 Error를 출력하는 함수
-
 void reporterror(const char* input) {
 	cErrors++;
 	int len = strlen(input);
 
 	// error type 판단
 	if (len > 10)
-		et = OVERLEN;				// [case 1] identifier가 10자 초과인 경우 -> overlen
+		identifier_et = OVERLEN;				// [case 1] identifier가 10자 초과인 경우 -> overlen
 	else if (isDigit(input[0]))		// [case 2] 숫자로 시작하는 경우 -> illid
-		et = ILLID;
+		identifier_et = ILLID;
 	else
-		et = ILLCHAR;				// [case 3] 특수문자가 존재하는 경우 -> illchar
+		identifier_et = ILLCHAR;				// [case 3] 특수문자가 존재하는 경우 -> illchar
 
 
-	printf("%d		**Error**					", currlinenum);
+	printf("[LEX ERROR]  ");
 
 	// error message 출력
-	switch (et) {
+	switch (identifier_et) {
 	case OVERLEN:
-		printf("%s TOO LONG \n", input);
+		printf("\"%s\" is too long", input);
 		break;
 	case ILLID:
-		printf("%s ILLEGAL IDENT \n", input);
+		printf("\"%s\" is an illegal identifier", input);
 		break;
 	case ILLCHAR:
 		for (int i = 0; i < len; i++) {
 			if (!(isDigit(input[i]) || isLetter(input[i])))
-				printf("%c ", input[i]);	// illegal character 출력
+				printf("\"%c\" ", input[i]);	// illegal character 출력
 		}
-		printf(" ILLEGAL CHARACTER \n");
+		printf(" is illegal character");
 		break;
 	}
+	printf(" (line %d)\n", currlinenum);
 }
 
-void yyerror(char* s)
+void yyerror(char* s) {}
+
+void printerror(char* non_terminal, enum errortypes et)
 {
-	printf("%s\n", s);
-	cErrors++; // 에러 수 세기 추가
+	cErrors++;
+	printf("[PARSE ERROR] ");
+
+	// error message 출력
+	switch (et) {
+	case NO_SEMI:
+		printf("no semicolon");
+		break;
+	case NO_EXP:
+		printf("no expression");
+		break;
+	case NO_NUM:
+		printf("no number");
+		break;
+	case NO_LEFTBRACKET:
+		printf("no left bracket");
+		break;
+	case NO_RIGHTBRACKET:
+		printf("no right bracket");
+		break;
+	case NO_LEFTBRACE:
+		printf("no left brace");
+		break;
+	case NO_RIGHTBRACE:
+		printf("no right brace");
+		break;
+	case NO_LEFTPAR:
+		printf("no left parenthesis");
+		break;
+	case NO_RIGHTPAR:
+		printf("no right parenthesis");
+		break;
+	}
+
+	printf("  (%s, line % d)\n", non_terminal, currlinenum - 1);
 }

8b85b857bf95467bb0d605546a8d3b0b3a131622
Eunbi <108458468+eunbi-jeong@users.noreply.github.com>
2023-05-31 23:50:02 +0900

    EB: Update comments

--- Assignment03/main.c
+++ Assignment03/main.c
@@ -1,5 +1,5 @@
 /*
-* main.c - Modified MiniC의 syntax analyzing을 위한 main 함수
+* main.c - Modified MiniC의 구문 분석을 위한 main 함수
 * programmer – 김민서, 정은비, 최민교
 * date – 30/05/2023
 */
@@ -18,9 +18,9 @@ extern LookupHS();
 extern PrintHStable();
 extern char* yytext;
 
-unsigned int cErrors = 0;			// 발생한 에러의 횟수 초기화
-unsigned int currlinenum = 1;       // 현재 토큰이 선언된 위치의 line number 초기화
-int cReturntype = -1;       // 현재 토큰이 선언된 위치의 line number 초기화
+unsigned int cErrors = 0;		// 발생한 에러의 횟수 초기화
+unsigned int currlinenum = 1;       	// 현재 토큰이 선언된 위치의 line number 초기화
+int cReturntype = -1;       		// function name의 return type 초기화
 
 // main 함수 
 void main()
@@ -30,6 +30,6 @@ void main()
 	printf("\n*************************** end of parser ***************************** \n\n");
 
 	printf("-> %d errors detected\n", cErrors);	// 에러가 발생한 횟수 출력
-	PrintHStable();									// Hashtable 출력
+	PrintHStable();									// Hash Table 출력
 	printf("\n[1983024 Mingyo Choi, 2017007 Minseo Kim, 2173109 Eunbi Jeong]\n");
 }
